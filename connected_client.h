3a2269c03974dab9a3bafbc351eb71a3887b681a
ViniciusMeirelles <vinicius.meirelles@outlook.com.br>
2019-05-08 17:24:17 -0300

    created a class to represent the client connected to the server. Changes to the server.

---
+++ connected_client.h
@@ -0,0 +1,31 @@
+#ifndef CONNECTED_CLIENT_H
+#define CONNECTED_CLIENT_H
+
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <pthread.h>
+
+using namespace std;
+
+class Connected_client{
+
+	public:
+		Connected_client(pthread_t thread, string username, int sockfd);
+
+		int get_sockfd();
+		int get_num_connections();
+
+		// Returns 0 if num_connections+1 < max_connections, -1 otherwise
+		int new_connection();
+
+	private:
+		string username;
+		int sockfd;
+		int num_connections;
+		int max_connections;
+		pthread_t thread;
+
+};
+
+#endif // CONNECTED_CLIENT_H

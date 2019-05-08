b85d6cfa107e8efd03929218e4b817a3d687c695
Carolina <carollangemello@gmail.com>
2019-05-08 15:31:40 -0300

    Atualizando o client com criação do diretório

--- client.cpp
+++ client.cpp
@@ -1,11 +1,13 @@
 #include "client.h"
 
 #include <iostream>
-using namespace std;
+#include <stdlib.h>
+#include <sys/stat.h>
 
 Client::Client(std::string username){
     this->username = username;
     this->isLogged = false;
+    this->dir = " ";
 }
 
 std::string Client::getUsername() {
@@ -20,6 +22,31 @@ bool Client::getIsLogged() {
     return this->isLogged;
 }
 
+void Client::setDir(std::string dir) {
+    this->dir = dir;
+}
+
+// Pretendo mudar pra outra classe tipo um Folder ou Util
+std::string Client::createSyncDir() {
+    // filesystem path
+    const char *homedir = getenv("HOME");
+    std::string syncdir = "/sync_dir_" + getUsername();
+    std::string dir = std::string(homedir) + syncdir;
+    //std::string print;
+    //std::cout << print << path;
+
+	const char* folder = dir.c_str();
+    struct stat info;
+    if (stat(folder, &info) == 0 && S_ISDIR(info.st_mode)){
+        std::cout << "Found folder." << std::endl;
+    } else {
+		// Read + Write + Execute:  S_IRWXU(user), S_IRWXG(group), S_IRWXO(others)
+        mkdir(folder, S_IRWXU | S_IRWXG | S_IRWXO);
+        std::cout << "Didn't found folder,\ncreating one:\n\t" << dir << std::endl;
+    }
+	return dir;
+}
+
 void Client::userInterface() {
     bool running = true;
     std::string input;
@@ -28,9 +55,6 @@ void Client::userInterface() {
     while(running) {
         std::getline(std::cin, input);
         command = input.substr(0, input.find(" "));
-
-
-        // SWITCH NAO ACEITA STRING, FAZ UM IF
 
         if(command == "upload") {
             std::cout << "Upload \n";
@@ -75,12 +99,22 @@ int main(int argc, char **argv) {
 	}
 
 	std::string username = argv[1];
+   	std::string host = argv[2];
+   	int port = atoi(argv[3]); // de repente pode ser um uint16?
 
 	Client client = Client(username);
 
-	// CONEXÃO COM O SERVIDOR
+	/*** CONEXÃO COM O SERVIDOR ***/
+	
+    // algo tipo: client.connect();
 
-	// SINCRONIZAÇÃO COM O SERVIDOR
+	/*** SINCRONIZAÇÃO COM O SERVIDOR ***/
+    // Aqui verifica se já existe o diretório, se não existe então cria
+    std::string dir = client.createSyncDir();
+	client.setDir(dir);
+
+	//dai aqui sincroniza com o servidor
+
 
     /*
     se conseguir conectar e sincronizar entao ta logado,
@@ -88,7 +122,8 @@ int main(int argc, char **argv) {
     client.setIsLogged(true)
     */
 
-    // interface com o usuário
+	// check_files -> pthread
+
+    /*** INTERFACE COM O USUARIO ***/
     client.userInterface();
 }
-
--- client.h
+++ client.h
@@ -1,17 +1,25 @@
+#include "communication_client.h"
+
 #include <string>
 
 class Client {
 private:
     std::string username; // userId
     bool isLogged;
+    std::string dir;
 
 public:
     Client(std::string username);
 
+    // gets e sets
     void setUsername(std::string username);
     std::string getUsername();
     void setIsLogged(bool isLogged);
     bool getIsLogged();
+    void setDir(std::string dir);
+
+    // rest
+    std::string createSyncDir();
     void userInterface();
 };
 

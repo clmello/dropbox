3c9f4ce7063b54a0316c79e1e53e9b42a636556c
jadyfeijo <42940544+jadyfeijo@users.noreply.github.com>
2019-06-23 03:25:34 -0300

    Eleição

---
+++ Eleição/Valentao.h
@@ -0,0 +1,18 @@
+#ifndef BULLY
+#define BULLY
+#include <iostream>
+#include "Pacote.h"
+#include "Cliente.h"
+
+using namespace std;
+
+    int contMSG =1;
+    bool Lider =false;
+
+    void lider(Pacote pacote);
+    void resto(Pacote pacote);
+    void eleicao(int sourceID);
+
+
+
+#endif
